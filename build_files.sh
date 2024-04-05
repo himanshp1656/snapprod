df53ef6ce4d831af578c0ad1e01ee64c25bc0314
himanshu pandey <0221csai057@niet.co.in>
2024-04-05 18:56:14 +0530

    Your commit message

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,21 @@
-echo " BUILD START"
+#!/bin/bash
+
+# Update pip
+echo "Updating pip..."
+python3.9 pip install -U pip
+
+# Install dependencies
+
+echo "Installing project dependencies..."
 python3.9 -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+# Collect staticfiles
+echo "Collect static..."
 python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
+
+echo "Build process completed!"
